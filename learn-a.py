9a74718e17528d058a08e180349e6a9e31877e21
Afida Rindy <afidarindy3@gmail.com>
2022-11-16 13:47:42 +0700

    new learn-a

---
+++ learn-a.py
@@ -0,0 +1,10 @@
+# Learning Python with Afida <3 Part 2
+
+# Here you'll find :
+# 1. How to add input..
+# 2. ..and print the output.
+
+yourName    = input("Hi, there! What's your name?")
+yourAddress    = input("Great! So, where do you live?")
+
+print('Alright, ' + yourName + '. I guess ' + yourAddress + ' is not too far from here. I hope we can get along :)')
