b9890705bd0be0f0024f760e0998c11ed8c4da42
Afida Rindy <afidarindy3@gmail.com>
2022-11-16 15:37:20 +0700

    new learn-c

---
+++ learn-c.py
@@ -0,0 +1,12 @@
+# Learning Python with Afida <3 Part 3
+
+# Here you'll find :
+# 1. While loop
+# 2. For loop
+
+sky   = 7
+while sky >= 1:    
+    print (sky, ' floor')
+    sky = sky - 1
+else:
+    print ('-basement-')
