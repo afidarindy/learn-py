cb1a10e0788386dc319cc611e94750b83eabc9a1
Afida Rindy <afidarindy3@gmail.com>
2022-11-16 11:28:29 +0700

    new index

---
+++ index.py
@@ -0,0 +1,54 @@
+# Learning Python with Afida <3
+
+# Here you'll find :
+# 1. Data types
+# 2. Concatenation
+# 3. Escaping from certain characters
+# 4. Line break
+# 5. 
+
+print("Print a string with quotation marks")
+print('Print a string with apostrophe')
+
+print('--------------------------------------')
+
+print('1. Data Types')
+
+yourVar = 'This is string in a defined variable'
+print(yourVar)
+
+x = 1104
+print(x)
+
+print('--------------------------------------')
+
+print('2. Concatenate two or more strings :')
+
+firstName   = 'Afida'
+lastName    = 'Annisa'
+print (firstName + ' Rindy ' + lastName)
+
+print('--------------------------------------')
+
+print('3. Escaping characters/symbols :')
+
+print (firstName + " \"Rindy\" " + lastName)
+print (firstName + " \'Rindy\' " + lastName)
+
+
+print('--------------------------------------')
+
+print('4. Adding a break/new line :')
+
+stepOne = '  One : Close your eyes'
+stepTwo = '  Two : Count all the blessings you have'
+print (stepOne + '\n' + stepTwo)
+
+print('--------------------------------------')
+
+print('5. Playing with number :')
+
+#Capitalized
+print('Capitalized method')
+c = "mushroom"
+c.capitalize()
