53009b274d86c51915ab442c29003d0591b91773
Afida Rindy <afidarindy3@gmail.com>
2022-12-01 15:57:15 +0700

    done for e dictionary

--- learn-e.py
+++ learn-e.py
@@ -1,4 +1,30 @@
 # Learning Python with Afida <3 Part 4
 
 # Here you'll find :
-# 1. Lists - Tuples
+# 1. Dictionary
+# 2. Nested dictionary
+
+data = {
+    'name'      : 'myosotis',
+    'aliases'   : 'forget me not/scorpion grasses',
+    'type'      : 'flower',
+    'size'      : '15',
+    'meaning'   : 'a testament to your relationships and promises the other person that you will never forget them in your thoughts'
+}
+
+print('The ' + data['name'] + ' ' + data['type'] + ' also known as ' + data['aliases'])
+
+# Add data maxSize
+data['maxSize'] = 60
+print(data)
+
+# Update name to Myositis
+data['name'] = 'Myositis'
+print(data)
+
+# Delete maxSize data
+del data['maxSize']
+print(data)
+
+for key, value in data.items():
+    print(key + ' - ' + value + '\n')
